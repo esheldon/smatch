6532ab97ea081df67b7b0735406f87c0ccb442c7
Matthew R. Becker <becker.mr@gmail.com>
2021-03-18 15:47:35 -0500

    ENH add tests; use setuptools; add license

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-import distutils
-from distutils.core import setup, Extension, Command
-import os
+from setuptools import setup, Extension
 import numpy
 
 
-ext=Extension("smatch._smatch", 
-              ["smatch/smatch.c",
-               "smatch/vector.c",
-               "smatch/tree.c",
-               "smatch/cat.c",
-               "smatch/healpix.c"])
-setup(name="smatch", 
-      packages=['smatch'],
-      version="0.9.0.1",
-      ext_modules=[ext],
-      include_dirs=numpy.get_include())
+ext = Extension(
+    "smatch._smatch",
+    [
+        "smatch/smatch.c",
+        "smatch/vector.c",
+        "smatch/tree.c",
+        "smatch/cat.c",
+        "smatch/healpix.c",
+    ])
 
-
-
-
+setup(
+    name="smatch",
+    packages=['smatch'],
+    version="0.9.0.1",
+    ext_modules=[ext],
+    include_dirs=numpy.get_include(),
+)
